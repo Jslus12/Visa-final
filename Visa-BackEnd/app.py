49ed075883f9a37c2ab6b7f859024523ef191dfd
Rafael Koti <kotynnn5@gmail.com>
2026-05-25 19:55:58 -0300

    Arrumando as rotas, por que um arrombado fez errado

--- Visa-BackEnd/app.py
+++ Visa-BackEnd/app.py
@@ -5,8 +5,8 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 app = Flask(
     __name__,
-    template_folder=os.path.join(BASE_DIR, '../templates'),
-    static_folder=os.path.join(BASE_DIR, '../static')
+    template_folder=os.path.join(BASE_DIR, '../Visa-FrontEnd/templates'),
+    static_folder=os.path.join(BASE_DIR, '../Visa-FrontEnd/static')
 )
 
 @app.route('/')
